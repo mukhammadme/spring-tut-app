df73bc7c56c18f3f7d59720742d79b1af6e27dbd
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-20 15:25:39 +0500

    Spring Tutorial 13 - Bean Definition Inheritance

--- src/com/linc/Triangle.java
+++ src/com/linc/Triangle.java
@@ -5,41 +5,25 @@ import org.springframework.beans.factory.BeanNameAware;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 
+import java.util.List;
+
 public class Triangle implements ApplicationContextAware, BeanNameAware {
-    private Point pointA;
-    private Point pointB;
-    private Point pointC;
+    private List<Point> points;
     private ApplicationContext applicationContext;
     private String beanName;
 
-    public Point getPointA() {
-        return pointA;
+    public List<Point> getPoints() {
+        return points;
     }
 
-    public void setPointA(Point pointA) {
-        this.pointA = pointA;
-    }
-
-    public Point getPointB() {
-        return pointB;
-    }
-
-    public void setPointB(Point pointB) {
-        this.pointB = pointB;
-    }
-
-    public Point getPointC() {
-        return pointC;
-    }
-
-    public void setPointC(Point pointC) {
-        this.pointC = pointC;
+    public void setPoints(List<Point> points) {
+        this.points = points;
     }
 
     public void draw() {
-        System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
-        System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
-        System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+        for (Point point : this.points) {
+            System.out.println("Point = (" + point.getX() + ", " + point.getY() + ")");
+        }
     }
 
     @Override
