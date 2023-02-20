e46d913eda6be0c23ddb1e9192613b5052210b20
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-20 14:38:48 +0500

    Spring Tutorial 12 - Using ApplicationContextAware

--- src/com/linc/Triangle.java
+++ src/com/linc/Triangle.java
@@ -1,9 +1,16 @@
 package com.linc;
 
-public class Triangle {
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanNameAware;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+
+public class Triangle implements ApplicationContextAware, BeanNameAware {
     private Point pointA;
     private Point pointB;
     private Point pointC;
+    private ApplicationContext applicationContext;
+    private String beanName;
 
     public Point getPointA() {
         return pointA;
@@ -33,5 +40,16 @@ public class Triangle {
         System.out.println("Point A = (" + getPointA().getX() + ", " + getPointA().getY() + ")");
         System.out.println("Point B = (" + getPointB().getX() + ", " + getPointB().getY() + ")");
         System.out.println("Point C = (" + getPointC().getX() + ", " + getPointC().getY() + ")");
+    }
+
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    @Override
+    public void setBeanName(String name) {
+        this.beanName = name;
+        System.out.println("Bean Name: " + this.beanName);
     }
 }
