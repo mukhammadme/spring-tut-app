8608eeb442708d2d3bef8654fa527d1a14111f3b
Mukhammad Pardaboev <mukhammad.pardaboev@sap.com>
2023-02-22 09:01:45 +0500

    Spring Tutorial 19 - The Autowired Annotation

--- src/com/linc/Circle.java
+++ src/com/linc/Circle.java
@@ -1,6 +1,7 @@
 package com.linc;
 
-import org.springframework.beans.factory.annotation.Required;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 
 public class Circle implements Shape {
     private Point center;
@@ -9,7 +10,8 @@ public class Circle implements Shape {
         return center;
     }
 
-    @Required
+    @Autowired
+    @Qualifier(value = "circleRelated")
     public void setCenter(Point center) {
         this.center = center;
     }
